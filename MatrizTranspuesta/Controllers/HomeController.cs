1e6298c92f7f848554ac5da59aa98aa08dcd95d8
hrueda15 <hrueda15@hotmail.com>
2015-03-26 09:48:06 -0430

    Actualización de Proyecto
    Actualización de prueba

---
+++ MatrizTranspuesta/Controllers/HomeController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MatrizTranspuesta.Models;
+
+
+namespace MatrizTranspuesta.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = Tittle.NameProyect;
+           
+            ViewBag.Success = false;
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult UploadFile()
+        {
+            HttpPostedFileBase file = null;
+
+              ViewBag.Success = false;
+                if (Request.Files["UploadedFile"].ContentLength > 0)
+                {
+                    string extension = System.IO.Path.GetExtension(Request.Files["UploadedFile"].FileName);
+                    if (extension == ".txt")
+                   {                       
+                         file = this.HttpContext.Request.Files.Get("UploadedFile");
+                       
+                        
+                        ViewBag.Success = true;
+                    }
+                    else
+                        ModelState.AddModelError(string.Empty, "Verifique, el archivo formato invalido");
+                }
+
+                return View((object)Convertir(file));
+        }
+
+
+        private string Convertir(HttpPostedFileBase file)
+        {
+            System.IO.StreamReader Model = new System.IO.StreamReader(file.InputStream);
+            System.IO.StreamReader Modeltemp = Model;
+            string[] stringSeparators = new string[] { "," };
+            const char CR = (char)13 ;
+            const char LF = (char)10;
+            int rowcount=0, columncount = 0;
+            String linea;
+            string[] columna;
+            List<String> row=new List<String>();
+            while (Model.Peek() != -1)
+            {
+            
+                linea = Model.ReadLine();
+                if (linea.Trim().Length > 0)
+                {
+                    row.Add( linea.Trim());
+                }
+            }        
+
+            rowcount = row[0].Split(stringSeparators, StringSplitOptions.None).Count();
+            columncount = row.Count();
+            string[,] Arreglo = new string[rowcount, columncount];
+            String ModelResult=string.Empty ; 
+            string temp ;
+
+            if (rowcount>columncount)
+                {
+                    for (int i = 0; i < columncount; i++)
+                    {
+                        for (int j =  0; j <= columncount +1; j++)
+                        {
+                            columna=row[i].Split(stringSeparators, StringSplitOptions.None);
+                            temp = columna[j];
+                            Arreglo[j,i]=temp;
+
+                            if (j < columncount )
+                            {
+                                columna = row[j].Split(stringSeparators, StringSplitOptions.None);
+                                temp = columna[i];
+                                Arreglo[i, j] = temp;
+                            }
+                    
+                            }
+                        }
+                }
+            else if (rowcount < columncount)
+            {
+                for (int i = 0; i < rowcount; i++)
+                {
+                    for (int j = 0; j <= rowcount + 1; j++)
+                    {
+                        if (j < rowcount)
+                        {
+                            columna = row[i].Split(stringSeparators, StringSplitOptions.None);
+                            temp = columna[j];
+                            Arreglo[j, i] = temp;
+                        }
+
+                        columna = row[j].Split(stringSeparators, StringSplitOptions.None);
+                        temp = columna[i];
+                        Arreglo[i, j] = temp;
+
+                    }
+                }
+            }
+            else 
+            {
+                for (int i = 0; i < rowcount; i++)
+                {
+                    for (int j = 0; j <= rowcount + 1; j++)
+                    {
+                        if (j < rowcount)
+                        {
+                            columna = row[i].Split(stringSeparators, StringSplitOptions.None);
+                            temp = columna[j];
+                            Arreglo[j, i] = temp;
+
+                            columna = row[j].Split(stringSeparators, StringSplitOptions.None);
+                            temp = columna[i];
+                            Arreglo[i, j] = temp;
+                        }
+
+                        
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < rowcount; i++)
+            {
+                for (int j = 0; j < columncount ; j++)
+                {
+                    ModelResult = ModelResult + Arreglo[i, j];
+                    if (j < columncount-1)
+                        ModelResult = ModelResult + stringSeparators[0];
+                }
+                ModelResult = ModelResult + CR + LF + CR + LF;
+            }
+            return ModelResult.Trim();
+        }
+
+    }
+
+ 
+}
---
+++ MatrizTranspuesta/Models/Tittle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatrizTranspuesta.Models
+{
+    public static class Tittle
+    {
+        #region Atributos
+            public const string NameProyect = "Matriz Transpuesta";
+            public const string NameProcess = "Proceso de " + NameProyect;
+            public const string NameInfoProcess = "Se deben Realizar los siguiente pasos para la conversion matricial";
+            public const string NameStepsOne = "Seleccionar el archivo (txt)";
+            public const string NameStepsTwo = "Presionar el Boton para realizar la conversión";
+            public const string NameInfoResult = "Resultado de la conversión";
+        
+        #endregion
+       
+    }
+}
